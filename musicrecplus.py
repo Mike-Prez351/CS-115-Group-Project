7af5c27cf85b1e3f7cb9ea33b2f840800650622f
Charlie Villa <107364489+CharlesBVilla@users.noreply.github.com>
2023-11-28 19:01:50 -0500

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -4,11 +4,13 @@
 # CS 115 - Group Project                                                       #
 ################################################################################
 
+PREF_FILE = open("musicrecplus.txt", 'a')
+
 def loadUsers(fileName):
     '''Reads in a file of stored users' preferences stored in the file
     fileName. Returns a dictionary containing a mapping of user names to a list
     of preferred artists. Written by Michael.'''
-    file = open(fileName, 'r')
+    file = open("musicrecplus.txt", 'r')
     userLikes = {}
     for line in file:
         [user, likes] = line.strip().split(':')
@@ -18,7 +20,7 @@ def loadUsers(fileName):
     file.close()
     return userLikes
 
-def enterPreferences(user, prefs):
+def enterPreferences(user, userList):
     """
     takes username and list of prefs, writes to file in this syntax:
     username:artist1,artist2,artist3,...
@@ -45,18 +47,21 @@ def enterPreferences(user, prefs):
         with open("musicrecplus.txt", "a") as file:
             file.writelines(line)
 
-def getRecommendations():
-    '''Returns artists that the program recommends to the user. Written by 
-    Charles'''
-    mostSimilarUser = bestUser(currentUser, prefs, userList)
-    recommendations = notSimilar(prefs, userList[mostSimilarUser])
+def getRecommendations(currentUser, prefs, userList):
+    '''Returns artists that the program recommends to the user. It sets a variable called mostSimilarUser equal to the bestMatch function called with the parameters of the current user, prefs, and the userList to find the best and most similar user
+    to the currentUser. Then a variable called recommendations is made that takes the mostSimilarUser and puts them into the notSimilar function with L1 being the currentUser prefs and L2 being the mostSimilarUser list. This will end up being a new list of 
+    artists that are not in the currentUser prefs which is then returned(Charles)'''
+    mostSimilarUser = bestMatch(currentUser, prefs, userList)
+    recommendations = notMatch(prefs, userList[mostSimilarUser])
     return recommendations
 
-def bestUser(currentUser, userList, prefs):
-    """This function checks each user in the userList to see which one is the best match. The way it does this(Charles)"""
+def bestMatch(currentUser, userList, prefs):
+    """This function checks each user in the userList to see which one is the best match. The way it does this is listing out the users in userList since it uses keys() which will only return the names of the users.
+    Then it will loop through each one of those users and run that user through the matchCounter function to test and see if that user has the highest amount of matches. If that user has more matches than the previous 
+    highestMatches user, then it becomes the new highestMatches user and that user is assigned to the variable "best" which is returned at the end(Charles)"""
     users = list(userList.keys())
     highestMatches = -1
-    best = 
+    best = None
     for user in users: 
         matches = matchCounter(prefs, userList[user])
         if prefs not in userList[user]:
@@ -65,7 +70,7 @@ def bestUser(currentUser, userList, prefs):
                 best = user
     return best
 
-def notSimilar(L1,L2):
+def notMatch(L1,L2):
     """This function will check the two artist lists of the users for different artists. An empty list called L3 is made outside of the for loop, and the for loop itself loops through
     L2 to check if any given artist in L2 is in L1 or not, if an artist is found to be in L2 but not L1, then that artist will be added to L3(Charles)"""
     L3 = []
@@ -82,6 +87,35 @@ def matchCounter(L1,L2):
         if artist in L2: 
             matches += 1
     return matches
+
+def saveUserPreferences(user, prefs, userList[user], userList): 
+    """This will update the preferences that the currentUser had stored in prefs, and set it equal to the values of that user within userList, the user being the key to the prefs values. It then opens the file and 
+    writes within the file the new data and closes it(Charles)"""
+    userList[user] = prefs
+    file = open("musicrecplus.txt", "w")
+    for i in userList: 
+        save = str(i) + ":" + ",".join(userList[i]) + "\n"
+        file.write(save)
+    file.close()
+
+def temporaryDict(L): 
+    """This function creates a dictionary called tempDict which is a temporary dictionary that'll be used in the functions that look for the popularity stuff. The parameter of this function is a list L, and it will be 
+    looped through using a for loop and each time the element will be replaced with 0. Then tempDict is returned(Charles)"""
+    tempDict = {}
+    for i in L:
+        tempDict[i] = 0
+    return tempDict
+
+def artistList(userList):
+    """ff"""
+    original = []
+    filtered = []
+    for user in userList: 
+        original += userList[i]
+    for artist in original: 
+        if artist not in filtered: 
+            filtered += [artist]
+    return filtered
 
 def mostPopularArtists():
     '''Returns which artist is liked by the most users. Written by ____'''
@@ -121,6 +155,7 @@ def fileExists():
 
 def main():
     '''The main function of the program. Written by ______.'''
+    userList = loadUsers()
     fileExists()
     user = input('Please enter your name (put a $ symbol after your name if you wish your preferences to remain private): ')
     data = loadUsers('musicrecplus.txt')
